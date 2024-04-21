a7ca06ca4db810027d655a06a9677ca7d29538d7
lavishq <96654752+Lavishq@users.noreply.github.com>
2024-04-21 09:48:42 +0200

    index as response if uri '/'

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,25 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        // println!("Connection established!");
-
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // println!("Request: {:#?}", http_request);
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("index.html").unwrap();
+        let length = contents.len();
 
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // other req
+    }
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
 
 }
